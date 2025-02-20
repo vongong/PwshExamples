051ab595db59b352818fe3530643afe631c836f9
Von Gong <vgong@batteriesplus.com>
2025-02-20 13:33:23 -0600

    Add Ado Jwt

--- Azure/Get-AdoJwt.ps1
+++ Azure/Get-AdoJwt.ps1
@@ -1,16 +1,21 @@
+## Az cli
+$AdoResourceID = "499b84ac-1321-427f-aa17-267ca6975798"
+$accessToken = az account get-access-token --resource $AdoResourceID --query "accessToken" --output tsv
+Write-output "jwt: $accessToken"
 
+## Api
 $KvName = "Example-KeyVault-Dev"
 $AppName = "Example"
-
 $TenantId = Get-AzKeyVaultSecret -VaultName $KvName -Name "TenantId" -AsPlainText
 $ClientId = Get-AzKeyVaultSecret -VaultName $KvName -Name "$AppName-AppId" -AsPlainText
 $KeyId = Get-AzKeyVaultSecret -VaultName $KvName -Name "$AppName-Key" -AsPlainText
-$ApiResourceId = "https://management.azure.com/"
+$AdoResourceID = "499b84ac-1321-427f-aa17-267ca6975798"
 
 $headers = @{
     "Content-Type" = "application/x-www-form-urlencoded"
 }
-$body = "grant_type=client_credentials&resource=$([uri]::EscapeDataString($ApiResourceId))&client_id=$ClientId&client_secret=$KeyId"
+$body = "grant_type=client_credentials&resource=$AdoResourceID&client_id=$ClientId&client_secret=$KeyId"
+
 $response = Invoke-RestMethod "https://login.microsoftonline.com/$TenantId/oauth2/token" -Method 'POST' -Headers $headers -Body $body
 
 Write-Output "jwt: $($response.access_token)"
