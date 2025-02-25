610bf8924786cfd8b7c4839365eda2ab8a0f5b47
Von Gong <vgong@batteriesplus.com>
2025-02-25 13:20:01 -0600

    add DateTime

---
+++ Datetime/Basic.ps1
@@ -0,0 +1,24 @@
+
+# Get Current Date Time
+Get-Date
+
+# Date Time Components
+$now = Get-Date
+$now.Year
+$now.Month
+$now.Day
+$now.Hour
+$now.Minute
+$now.Second
+$now.Millisecond
+
+# Format Date
+Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+# Set Date
+$StartDate = [datetime]"02/25/2024 13:13"
+
+# Date Span
+$StartDate = [datetime]"01/01/2024 00:00"
+$EndDate = (GET-DATE)
+New-TimeSpan -Start $StartDate -End $EndDate
