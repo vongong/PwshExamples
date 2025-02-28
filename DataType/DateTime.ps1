5a4b450abdb2da4b1d91ed66daa970fbe37259ed
Von Gong <vgong@batteriesplus.com>
2025-02-28 08:45:41 -0600

    add to datetime

--- DataType/DateTime.ps1
+++ DataType/DateTime.ps1
@@ -1,8 +1,8 @@
 
-# Get Current Date Time
+## Get Current Date Time
 Get-Date
 
-# Date Time Components
+## Date Time Components
 $now = Get-Date
 $now.Year
 $now.Month
@@ -12,13 +12,18 @@ $now.Minute
 $now.Second
 $now.Millisecond
 
-# Format Date
+## Format Date
 Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-# Set Date
+## Set Date
 $StartDate = [datetime]"02/25/2024 13:13"
 
-# Date Span
+## Date Span
 $StartDate = [datetime]"01/01/2024 00:00"
 $EndDate = (GET-DATE)
 New-TimeSpan -Start $StartDate -End $EndDate
+
+## Add time
+# To add or subtract time, use the AddDays, AddHours, AddMinutes, or AddSeconds methods.
+(Get-Date).AddDays(1)
+(Get-Date).AddHours(-2)
